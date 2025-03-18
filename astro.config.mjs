a82622f313064023ce4b0fdaccc5dbe21a5c15b4
Konrad Karpieszuk <kkarpieszuk@gmail.com>
2025-03-18 21:17:13 +0100

    domain change

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config .
 export default defineConfig({
-    site: 'https://kkarpieszuk.github.io/gajowkapasieki.pl/',
+    site: 'http://gajowkapasieki.pl/',
     base: 'gajowkapasieki.pl',
 });
